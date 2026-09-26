175555f4e6a68642fa78d57cb4fadf39f004a929
Suchit das <dassuchit18@gmail.com>
2026-09-26 19:34:17 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int maxpro = 0;
+        for(int i = 1;i<prices.length ; i++){
+            if(prices[i]< min){
+                min = prices[i];
+            }
+            else maxpro = Math.max(maxpro,prices[i]-min);
+        }
+        return maxpro;
+    }
+}
